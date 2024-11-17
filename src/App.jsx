00ccf711a5709b55a37032935fe2a7ef9dae03b9
MH Code <mazheriftikhar1@gmail.com>
2024-11-17 15:32:11 +0500

    17 Nov changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Features from './Components/Features/Features'
 import Features1 from './Components/Features1/Features1'
 import Features2 from './Components/Features2/Features2'
 import Features3 from './Components/Features3/Features3'
+import Features4 from './Components/Features4/Features4'
+import FeatureSection16 from './Components/Features5/Features5'
+import Features6 from './Components/Features6/Features6'
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
       <Features1 />
       <Features2 />
       <Features3 />
+      <Features4 />
+      <FeatureSection16 />
+      <Features6 />
     </div>
   )
 }
