791bb7c595dee301f4434d3918ed74a5f3477058
MH Code <mazheriftikhar1@gmail.com>
2024-11-17 14:51:52 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Features from './Components/Features/Features'
+import Features1 from './Components/Features1/Features1'
+import Features2 from './Components/Features2/Features2'
+import Features3 from './Components/Features3/Features3'
+
+const App = () => {
+  return (
+    <div>
+      <Features />
+      <Features1 />
+      <Features2 />
+      <Features3 />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Features3/Features3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Features3 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Features3
